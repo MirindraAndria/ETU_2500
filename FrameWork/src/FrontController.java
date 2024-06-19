39360de41dc2e5174a6e527e2cd5414053d3bee1
MirindraAndria <mixandria19@gmail.com>
2024-06-19 06:33:26 +0300

    Framwork Sprint5
    Formulaire et Affichifage de donnee

---
+++ FrameWork/src/FrontController.java
@@ -0,0 +1,180 @@
+package servlet ; 
+
+import java.util.*;
+
+import annotation.AnnotationParam;
+import java.text.* ; 
+import java.io.* ; 
+import java.lang.reflect.* ;
+import mapping.* ; 
+import jakarta.servlet.* ; 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse; 
+import jakarta.servlet.http.HttpServletRequest;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import utility.Utility ; 
+import modelview.ModelView ; 
+import exception.* ;
+import java.lang.annotation.Annotation; 
+public class FrontController extends HttpServlet {
+    
+
+    private String Source ;  
+    private HashMap<String, Mapping> HashmapUtility =  new HashMap<>(); 
+    private Utility util = new Utility(); 
+
+    public void init() throws ServletException 
+    {
+        try
+        {
+            //Prendre  les controllers dans le WEB.xml 
+            this.Source = this.getInitParameter("Source")  ;
+            this.configMap(); 
+        }catch(Exception e )
+        {   
+                e.printStackTrace() ; 
+        }
+    } 
+
+    public boolean pathVerification(String path , String transformed)
+    {
+        if(path.equals(transformed))
+        {
+            return true ; 
+        }
+        return false ;
+    }
+
+    public String transformPath(String path) {
+        //Verifier si le dernier caracter est un "/" 
+        if (path.endsWith("/")) {
+            //suppression du "/""
+            path = path.substring(0, path.length() - 1);
+        }
+        String[] parts = path.split("/");
+        //Dernier element du path 
+        String lastElement = parts[parts.length - 1];
+        // Concatenange avec un point 
+        String transformed = "/" + lastElement ; 
+        return transformed;
+    }
+
+    public void configMap() throws Exception 
+    {
+        try{
+            ServletContext context = getServletContext() ; 
+            if( context.getResource(this.util.PathWithoutPackageName(this.Source)).getPath() != null){
+                String classpath = context.getResource(this.util.PathWithoutPackageName(this.Source)).getPath() ; 
+                String normalizedPath = this.util.normalizePath(classpath);   
+                String packageName = this.util.transformPath(this.Source) ;   
+                this.util.AddMethodeAnnotation( normalizedPath , packageName , this.HashmapUtility) ; 
+            }else {  throw new Exception("Error package Scan verify our file xml \n") ;  } 
+        }catch(Exception e )
+        { e.printStackTrace(); }   
+    } 
+
+    public void dispacthModelView( ModelView mv , HttpServletRequest request  ,  HttpServletResponse response )
+    {
+        try{
+            Set<String> keyMap= mv.getData().keySet(); 
+            for(String keymap : keyMap)
+            {
+                request.setAttribute( keymap , mv.getData().get(keymap)) ; 
+            }
+            RequestDispatcher dispatch = request.getRequestDispatcher( mv.getUrl());
+            dispatch.forward(request, response);
+        }catch(Exception e )
+        { 
+            System.out.println(e);
+        }
+    }
+
+    public ArrayList<Object> verifyCorrespondence( HttpServletRequest request  , Method myMethod   ) throws Exception
+    {
+    try{ 
+        ArrayList<Object> valueArg = new ArrayList<>() ; 
+        Parameter[] parameters = myMethod.getParameters();
+        for (Parameter parameter : parameters) {
+            Annotation paramAnnotations = parameter.getAnnotation( AnnotationParam.class) ;  
+            String paramName = parameter.getName();
+            if( paramAnnotations != null)
+            {  
+                AnnotationParam annotationParam = (AnnotationParam) paramAnnotations;
+                out.print(  "Request get : " + request.getParameter ( annotationParam.name() ) + "\n") ; 
+                valueArg.add( request.getParameter( annotationParam.name() ) ) ;  
+            }else { valueArg.add( request.getParameter( paramName ) ) ; } 
+        }
+        return valueArg ; 
+        }catch(Exception e )
+        {
+            e.printStackTrace() ;
+        }
+        return null ; 
+    } 
+
+
+    public void ShowResult(Mapping value  , PrintWriter out  , HttpServletRequest request  ,  HttpServletResponse response )throws TypeErrorException
+    {
+        try { 
+                out.print("Classe Name : " + value.getClasseName() + " , " + "Methode Name : " + value.getMethodeName() + "\n");
+                Class myClass = Class.forName(value.getClasseName());
+                Object myObject = myClass.getDeclaredConstructor(new Class[0]).newInstance(new Object[0]) ; 
+                Method myMethod = this.util.checkMethod(myClass, value.getMethodeName()  ) ;  
+                ArrayList<Object> valueArg = this.verifyCorrespondence( request , myMethod  ) ; 
+                Object res = this.util.invokingMethod( valueArg , myObject , myMethod ) ;        
+                if( res instanceof String)
+                {  out.print("Valeur de la methode String :" + res + "\n") ; } 
+                else if( res instanceof ModelView)
+                {   this.dispacthModelView( (ModelView)res , request , response); }
+                else { throw new TypeErrorException(" Error Type of return incorrect methode "); }   
+        }catch(Exception e )
+        {  e.printStackTrace() ; }
+        
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws TypeErrorException
+    {  
+        try{
+            PrintWriter out = response.getWriter() ; 
+            StringBuffer url = request.getRequestURL();
+            String urlString = url.toString();
+            String transformed = this.transformPath2(urlString) ;   
+            Mapping mapping = HashmapUtility.get(transformed);
+            if(mapping == null) {
+                throw new Exception("404 error , url incorrect");
+            }
+            else
+            { this.ShowResult(mapping  , out , request , response); }
+
+        }catch( Exception e ) 
+        {
+           System.out.println(e) ; 
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter() ; 
+
+        try{
+            processRequest(request, response);
+        }catch(Exception e){
+                e.printStackTrace(out) ;
+        }
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter() ; 
+        try{
+            processRequest(request, response);
+        }catch(Exception e )
+        {
+            e.printStackTrace(out) ;
+        }
+    }
+}
+
+
---
+++ FrameWork/src/Utility.java
@@ -0,0 +1,174 @@
+package utility ; 
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import exception.* ; 
+
+import mapping.Mapping;
+import modelview.ModelView;
+import annotation.* ; 
+
+public class Utility {
+    public Utility()
+    {
+
+    }
+    //L'instabiliter de tomcat modifie le path c'est la raison de cette fonction 
+    public String normalizePath(String path) {
+       //Enlever le premier / 
+        if (path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        // Remplace tous les % en espace 
+        try {
+            path = URLDecoder.decode(path, StandardCharsets.UTF_8.name());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return path;
+    }
+
+    public String PathWithoutPackageName(String path)
+    {
+            if (path.endsWith("/")) {
+                path = path.substring(0, path.length() - 1);
+            } 
+            // Find the last occurrence of '/'
+            int lastSlashIndex = path.lastIndexOf('/');
+            // Extract the substring up to the last '/'
+            String transformed = path.substring(0, lastSlashIndex + 1);
+            return transformed;
+    }
+    public boolean pathVerification(String path , String transformed)
+    {
+        if(path.equals(transformed))
+        {
+            return true ; 
+        }
+        return false ;
+    }
+
+    public String transformPath2(String path) {
+        //Verifier si le dernier caracter est un "/" 
+        if (path.endsWith("/")) {
+            //suppression du "/""
+            path = path.substring(0, path.length() - 1);
+        }
+        String[] parts = path.split("/");
+        //Dernier element du path 
+        String lastElement = parts[parts.length - 1];
+        // Concatenange avec un point 
+        String transformed = "/" + lastElement ; 
+        return transformed;
+    }
+
+
+    public String transformPath(String path) {
+        //Verifier si le dernier caracter est un "/" 
+        if (path.endsWith("/")) {
+            //suppression du "/""
+            path = path.substring(0, path.length() - 1);
+        }
+        String[] parts = path.split("/");
+        //Dernier element du path 
+        String lastElement = parts[parts.length - 1];
+        // Concatenange avec un point 
+        String transformed =  lastElement + "." ; 
+        return transformed;
+    } 
+
+    //Concatenage du package et classeName 
+    public String fusionPackageAndClassName(String className , String packageName)
+    {
+        String path =  packageName + className ; 
+        return path ; 
+    }
+    public void AddMethodeAnnotation( String normalizedPath , String packageName  , HashMap HashmapUtility) throws Exception 
+    {  
+        try {    
+            File classpathDirectory = new File(normalizedPath) ; 
+                    for ( File file : classpathDirectory.listFiles() )   
+                    {
+                        if(file.isFile() && file.getName().endsWith(".class"))
+                        {   
+                            String className = file.getName().substring( 0 , file.getName().length() - 6 ) ; 
+                            String trueClassName = this.fusionPackageAndClassName(className , packageName); 
+                            //Transformation en classe
+                            Class<?> myclass = Thread.currentThread().getContextClassLoader().loadClass(trueClassName) ; 
+                                if(myclass.isAnnotationPresent(AnnotationController.class))
+                                {
+                                        Method [] methods = myclass.getDeclaredMethods() ;
+                                        //Liste de Methode pour chaque Classe 
+                                        for (Method method : methods)
+                                            if(method.isAnnotationPresent(AnnotationGet.class))  
+                                            {
+                                                AnnotationGet annotation = method.getAnnotation(AnnotationGet.class);
+                                                String url = annotation.name();
+                                                Mapping mapping = new Mapping( trueClassName, method.getName() )  ;
+                                                //Ajout des information dans le Hashmap 
+                                                if(HashmapUtility.containsKey(url))
+                                                { throw new DuplicateKeyException("Error Annotation duplicated : " + url + "\n"); }
+                                                HashmapUtility.put( url , mapping ) ;
+                                            }
+                                }
+                        } 
+                    }
+        }catch( Exception e )
+        { e.printStackTrace(); } 
+    }
+
+      public Object invokingMethod( ArrayList<Object> argsList , Object myObject , Method myMethod   ) throws Exception
+     {
+        try{
+        // Adapter les arguments pour correspondre au nombre de paramètres de la méthode
+        Object[] args = null ; 
+        Object res = null ; 
+        if( myMethod.getParameterCount() == 0  ) 
+        {
+            res = myMethod.invoke( myObject , new Object[0]) ;
+            return res ; 
+        }
+        args = argsList.toArray() ; 
+        while (args.length < myMethod.getParameterCount()  ) {
+            args = Arrays.copyOf(args, args.length + 1);
+            args[args.length - 1] = null; // Remplacer par null les arguments manquant
+        }
+        res = myMethod.invoke(myObject, args); 
+        return res ; 
+        }catch(Exception e){ e.printStackTrace();} return null ; 
+    } 
+    public Method checkMethod( Class myClass , String methodName ) throws Exception 
+    {
+        try{
+        Method myMethod = null ; 
+        Method [] Allmethod = myClass.getDeclaredMethods()  ; 
+        for( int i = 0 ; i < Allmethod.length  ; i++ )
+        {
+            if( Allmethod[i].getName().equals(methodName) )
+            {
+                myMethod = Allmethod[i];
+                return myMethod ;  
+            }
+        }
+        if( myClass.getDeclaredMethod( methodName, new Class[0]) != null)
+        {  return  myClass.getDeclaredMethod( methodName, new Class[0])  ; }
+        return myMethod ; 
+        }catch(Exception e){
+            e.printStackTrace();
+        } return null ; 
+    }
+   
+}
+
+
